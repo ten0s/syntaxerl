65fc05bedc93e6fa1fe1ef04c391c25e383e9122
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2017-07-23 12:34:56 -0700

    Add -export([main/1]). to escipt module

--- src/syntaxerl_escript.erl
+++ src/syntaxerl_escript.erl
@@ -18,16 +18,13 @@
 check_syntax(FileName, BaseFileName, Debug) ->
     case file:read_file(FileName) of
         {ok, Content} ->
-            ShebangRepl =
-                case re:run(Content, <<"-module">>) of
-                    {match, _} ->
-                        %% module attribute is defined. just remote shebang
-                        <<>>;
-                    nomatch ->
-                        %% replace shebang line with module attribute
-                        <<"-module(fixed_escript).">>
+            Preface = <<"-module(fixed_escript).\n-export([main/1]).\n">>,
+            NewContent =
+                case binary:split(Content, <<"\n">>) of
+                    [<<"#!", _/binary>>, Content0] ->
+                        <<Preface/binary, "\n", Content0/binary>>;
+                    _Other -> <<Preface/binary, Content/binary>>
                 end,
-            NewContent = re:replace(Content, <<"#.*">>, ShebangRepl),
             NewFileName = filename:rootname(FileName, ".erl") ++ "_fixed.erl",
             case file:write_file(NewFileName, NewContent) of
                 ok ->
@@ -47,13 +44,18 @@ check_syntax(FileName, BaseFileName, Debug) ->
                         {ok, _ModuleName} ->
                             {ok, []};
                         {ok, _ModuleName, Warnings} ->
-                            {ok, syntaxerl_format:format_warnings(?MODULE, Warnings)};
+                            io:format("Warnings: ~p~n", [Warnings]),
+                            {ok, syntaxerl_format:format_warnings(
+                                    ?MODULE, fix_line_numbers(Warnings))};
                         {error, Errors, Warnings} ->
-                            case syntaxerl_format:format_errors(?MODULE, Errors) of
+                            case syntaxerl_format:format_errors(
+                                    ?MODULE, fix_line_numbers(Errors)) of
                                 [] ->
-                                    {ok, syntaxerl_format:format_warnings(?MODULE, Warnings)};
+                                    {ok, syntaxerl_format:format_warnings(
+                                            ?MODULE, fix_line_numbers(Warnings))};
                                 Errors2 ->
-                                    {error, Errors2 ++ syntaxerl_format:format_warnings(?MODULE, Warnings)}
+                                    {error, Errors2 ++ syntaxerl_format:format_warnings(
+                                        ?MODULE, fix_line_numbers(Warnings))}
                             end
                     end;
                 {error, Reason} ->
@@ -66,3 +68,22 @@ check_syntax(FileName, BaseFileName, Debug) ->
 output_error(_) -> true.
 
 output_warning(_) -> true.
+
+%% ===================================================================
+%% Internal
+%% ===================================================================
+
+fix_line_numbers(ErrorList) ->
+    ErrorList0 = skip_expected_errors(ErrorList),
+    [{F, [{fix_line_number(L), M, E} || {L, M, E} <- Es]}
+     || {F, Es} <- ErrorList0].
+
+%% appropriately fix line numbers due to the `-module' definition.
+fix_line_number(none) -> 1;
+fix_line_number(Line) when Line < 3 -> 1;
+fix_line_number(Line) -> Line - 2.
+
+skip_expected_errors(ErrorList) ->
+    [{F, lists:keydelete(redefine_module, 3,
+         lists:keydelete({duplicated_export, {main, 1}}, 3, Es))}
+     || {F, Es} <- ErrorList].
